995352d052e90d940d646405c84f76097be3bbc0
Alexer <robotalexjennings@gmail.com>
2024-10-28 18:53:58 -0500

    Update zpm-mac.sh

--- zpm-mac.sh
+++ zpm-mac.sh
@@ -29,8 +29,8 @@ echo "Built URL: $url"  # Print the constructed URL to stdout (or use it in a va
 download_url="$url"
 downloaded_file="$package_name.sh"
 curl -L "$download_url" -o "$downloaded_file"  # The -L option handles redirects
-chmod +x "$package_name.sh"
-sudo ln -s "$(pwd)/$package_name.sh" /usr/local/bin/$package_name"
+chmod +x "$downloaded_file"
+sudo ln -s "$(pwd)/$downloaded_file" /usr/local/bin/$package_name"
 echo "Installed $1"
 
 
